19514abd4435077380bfedf87725e0c26d088c65
MariVS0912 <mvargass3@eafit.edu.co>
2025-11-19 17:17:36 -0500

    Update mqtt_utils.py

--- Smart-Home/mqtt_utils.py
+++ Smart-Home/mqtt_utils.py
@@ -76,3 +76,8 @@ def publish_message(message):
 # -----------------------------
 def get_sensor_data():
     return sensor_data
+
+if "mqtt_started" not in st.session_state:
+    connect_mqtt()
+    st.session_state.mqtt_started = True
+
