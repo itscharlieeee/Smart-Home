061849c1c19d268de7ad19a1c8fe0ac2319288c5
MariVS0912 <mvargass3@eafit.edu.co>
2025-11-19 21:41:41 -0500

    Update 1-HomeStatus.py

--- Smart-Home/pages/1-HomeStatus.py
+++ Smart-Home/pages/1-HomeStatus.py
@@ -4,19 +4,26 @@ from mqtt_utils import get_sensor_data, get_device_status, publish_message
 
 st.header("Estado de la Casa")
 
-# Ejemplo de habitaciones y dispositivos
 habitaciones = {
-    "Sala": ["casa/luz/sala", "casa/enchufe/televisor"],
-    "Cocina": ["casa/luz/cocina", "casa/enchufe/licuadora"],
-    "Habitación": ["casa/luz/habitacion", "casa/enchufe/lampara"]
+    "Sala": [
+        "casa/luces/sala",
+        "casa/enchufe/televisor"
+    ],
+    "Cocina": [
+        "casa/luces/cocina"
+    ],
+    "Habitación": [
+        "casa/luces/habitacion",
+        "casa/enchufe/lampara"
+    ]
 }
 
 for hab, dispositivos in habitaciones.items():
-    with st.expander(f"{hab}"):
+    with st.expander(hab):
         for dev in dispositivos:
             estado = get_device_status(dev)
             st.write(f"{dev.split('/')[-1].capitalize()}: {estado}")
-            # Opcional: botón para encender/apagar desde Home Status
+
             if st.button(f"Tog {dev}", key=f"{dev}_toggle"):
-                nuevo_estado = "OFF" if estado=="ON" else "ON"
+                nuevo_estado = "OFF" if estado == "ON" else "ON"
                 publish_message(dev, nuevo_estado)
